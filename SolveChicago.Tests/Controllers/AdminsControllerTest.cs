1d92e92c036c5ac7af045fa148181d8e93b6a5bc
Reid Compton <reid@reidcompton.com>
2017-05-08 00:39:58 +0000

    fix unit tests

--- SolveChicago.Tests/Controllers/AdminsControllerTest.cs
+++ SolveChicago.Tests/Controllers/AdminsControllerTest.cs
@@ -18,7 +18,58 @@ namespace SolveChicago.Tests.Controllers
     {
         Mock<SolveChicagoEntities> context = new Mock<SolveChicagoEntities>();
 
-        public AdminsControllerTest()
+        public void SetupData()
+        {
+            List<AspNetUser> users = new List<AspNetUser>();
+            List<Admin> data = new List<Admin>
+            {
+                new Admin
+                {
+                    Id = 1,
+                    FirstName = "John",
+                    LastName = " Doe",
+                    AspNetUser = 
+                        new AspNetUser
+                        {
+                            Id = ")(*Y&T^RDTXFGCHVUJI"
+                        }
+                },
+                new Admin
+                {
+                    Id = 2,
+                    FirstName = "Sayid",
+                    LastName = "Khan",
+                    AspNetUser = 
+                        new AspNetUser
+                        {
+                            Id = "BHVGYCRD%$^&*()O"
+                        }
+                },
+                new Admin
+                {
+                    Id = 3,
+                    FirstName = "Jerry",
+                    LastName = "Ellis",
+                    AspNetUser = 
+                        new AspNetUser
+                        {
+                            Id = "HGSE#%$^&G&IUBH"
+                        }
+                }
+            };
+
+            var set = new Mock<DbSet<Admin>>().SetupData(data);
+            var userSet = new Mock<DbSet<AspNetUser>>().SetupData(users);
+            set.Setup(m => m.Find(It.IsAny<object[]>()))
+                .Returns<object[]>(ids => data.FirstOrDefault(d => d.Id == (int)ids[0]));
+            context.Setup(c => c.Admins).Returns(set.Object);
+            context.Setup(c => c.AspNetUsers).Returns(userSet.Object);
+
+        }
+        
+
+        [Fact]
+        public void Admin_Index()
         {
             List<AspNetUser> users = new List<AspNetUser>();
             List<Nonprofit> data = new List<Nonprofit>
@@ -33,7 +84,7 @@ namespace SolveChicago.Tests.Controllers
                         {
                             Id = ")(*Y&T^RDTXFGCHVUJI"
                         }
-                    }                        
+                    }
                 },
                 new Nonprofit
                 {
@@ -68,12 +119,6 @@ namespace SolveChicago.Tests.Controllers
             context.Setup(c => c.Nonprofits).Returns(set.Object);
             context.Setup(c => c.AspNetUsers).Returns(userSet.Object);
 
-        }
-        
-
-        [Fact]
-        public void Admin_Index()
-        {
             AdminsController controller = new AdminsController(context.Object);
             var result = (ViewResult)controller.Index();
 
@@ -86,6 +131,7 @@ namespace SolveChicago.Tests.Controllers
         [Fact]
         public void AdminsController_Details_ReturnsBadRequest()
         {
+            SetupData();
             AdminsController controller = new AdminsController(context.Object);
             var result = (HttpStatusCodeResult)controller.Details(null);
 
@@ -95,6 +141,7 @@ namespace SolveChicago.Tests.Controllers
         [Fact]
         public void AdminsController_Details_ReturnsNotFound()
         {
+            SetupData();
             AdminsController controller = new AdminsController(context.Object);
             var result = (HttpStatusCodeResult)controller.Details(0);
 
@@ -104,6 +151,7 @@ namespace SolveChicago.Tests.Controllers
         [Fact]
         public void AdminsController_Details_ReturnsAdmin()
         {
+            SetupData();
             AdminsController controller = new AdminsController(context.Object);
             var result = (ViewResult)controller.Details(3);
 
@@ -116,6 +164,7 @@ namespace SolveChicago.Tests.Controllers
         [Fact]
         public void AdminsController_Create_GET_ReturnsViewResult()
         {
+            SetupData();
             AdminsController controller = new AdminsController(context.Object);
             var result = (ViewResult)controller.Create();
             var viewResult = Assert.IsType<ViewResult>(result);
@@ -124,6 +173,7 @@ namespace SolveChicago.Tests.Controllers
         [Fact]
         public void AdminsController_Create_POST_ReturnsAdmin()
         {
+            SetupData();
             Admin admin = new Admin
             {
                 CreatedDate = DateTime.UtcNow,
