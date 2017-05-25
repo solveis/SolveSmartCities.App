b5cd3cec9adf7fc4c13f27472918415dd90a2ad6
Reid Compton <reid@reidcompton.com>
2017-05-25 22:03:48 +0000

    schema changes #75

---
+++ SolveChicago.Db/dbo/Tables/NonprofitPhoneNumbers.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[NonprofitPhoneNumbers]
+(
+	[NonprofitId] INT NOT NULL,
+    [PhoneNumberId] INT NOT NULL,
+	
+    PRIMARY KEY ([NonprofitId], [PhoneNumberId]), 
+    CONSTRAINT [FK_NonprofitPhoneNumbers_Nonprofits] FOREIGN KEY ([NonprofitId]) REFERENCES [Nonprofits]([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_NonprofitPhoneNumbers_PhoneNumber] FOREIGN KEY (PhoneNumberId) REFERENCES [PhoneNumbers]([Id]) ON DELETE CASCADE,
+)
